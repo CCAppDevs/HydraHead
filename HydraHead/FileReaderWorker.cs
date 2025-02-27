cda77ae411b1c30ad1bad51347366d4ec9bc3a98
Jesse Harlan <jesse.harlan@centralia.edu>
2025-02-27 12:35:38 -0800

    added file head and command line args

---
+++ HydraHead/FileReaderWorker.cs
@@ -0,0 +1,70 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydraHead
+{
+    public class FileReaderWorker : BackgroundService
+    {
+        private string _path;
+        private int _delay;
+        private readonly ILogger<FileReaderWorker> _logger;
+
+        public FileReaderWorker(ILogger<FileReaderWorker> logger)
+        {
+            // setup any content we need
+            _path = Path.Combine(Directory.GetCurrentDirectory(), "Files", "Test.txt");
+            _delay = 1000;
+            _logger = logger;
+        }
+
+        protected string? ScanForToken(string token)
+        {
+            string contents = File.ReadAllText(_path);
+
+            string? foundLine = null;
+
+            foreach (var line in contents.Split("\n"))
+            {
+                if (line.StartsWith(token))
+                {
+                    foundLine = line;
+                    break;
+                }
+            }
+
+            return foundLine;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var token = "[ERROR]";
+                var foundError = ScanForToken(token);
+
+                if(foundError == null)
+                {
+                    _logger.LogInformation("No Errors Found.");
+                }
+                else
+                {
+                    // tell the hydra api what we found
+                    _logger.LogCritical(
+                        $"Found File at path: {_path}\n" +
+                        $"\tToken: {token}\n" +
+                        $"\tLine: {foundError}\n"
+                    );
+
+                    // log the event with the api.
+                }
+
+                // close the file
+                await Task.Delay(_delay);
+            }
+        }
+    }
+}
--- HydraHead/Program.cs
+++ HydraHead/Program.cs
@@ -6,11 +6,26 @@ namespace HydraHead
     {
         public static void Main(string[] args)
         {
+            // C:\HydraHead.exe file c:\Path\To\Filename.txt
+            Console.WriteLine("HydraHead executed with these arguments:");
+            foreach (string item in args)
+            {
+                Console.WriteLine(item);
+            }
+            
             var builder = Host.CreateApplicationBuilder(args);
             builder.Services.AddSingleton<HydraApiService>();
-            //builder.Services.AddSingleton<JokeService>(js => new JokeService("https://geek-jokes.sameerkumar.website"));
-            builder.Services.AddSingleton<JokeService>(js => new JokeService("https://localhost"));
-            builder.Services.AddHostedService<JokeAPIWorker>();
+            
+            // determine which head to start
+            if (args[0] == "file")
+            {
+                builder.Services.AddHostedService<FileReaderWorker>();
+            }
+            else if (args[0] == "joke")
+            {
+                builder.Services.AddSingleton<JokeService>(js => new JokeService("https://geek-jokes.sameerkumar.website"));
+                builder.Services.AddHostedService<JokeAPIWorker>();
+            }
 
             var host = builder.Build();
             host.Run();
