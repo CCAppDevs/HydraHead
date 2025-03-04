e501edddf473cbe31aeacdd8aee724ed84237209
Jesse Harlan <jesse.harlan@centralia.edu>
2025-03-04 12:39:02 -0800

    added orchestration support

--- HydraHead/FileReaderWorker.cs
+++ HydraHead/FileReaderWorker.cs
@@ -12,13 +12,15 @@ namespace HydraHead
         private string _path;
         private int _delay;
         private readonly ILogger<FileReaderWorker> _logger;
+        private readonly HydraApiService _hydra;
 
-        public FileReaderWorker(ILogger<FileReaderWorker> logger)
+        public FileReaderWorker(ILogger<FileReaderWorker> logger, HydraApiService hydra)
         {
             // setup any content we need
             _path = Path.Combine(Directory.GetCurrentDirectory(), "Files", "Test.txt");
-            _delay = 1000;
+            _delay = 10000;
             _logger = logger;
+            _hydra = hydra;
         }
 
         protected string? ScanForToken(string token)
@@ -59,7 +61,25 @@ namespace HydraHead
                         $"\tLine: {foundError}\n"
                     );
 
-                    // log the event with the api.
+
+                    // create an event
+                    var evt = new Event
+                    {
+                        EventId = 0,
+                        Description = foundError,
+                        Type = EventType.LogFileMessage,
+                        CategoryId = 2,
+                        Timestamp = DateTime.Now,
+                        HostAddress = Environment.MachineName
+                    };
+
+                    // call the api with the event
+                    var result = await _hydra.Post(evt);
+
+                    if (result == null)
+                    {
+                        throw new HttpRequestException("Hydra Unavailable");
+                    }
                 }
 
                 // close the file
